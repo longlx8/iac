c08573ebcbe041315abbafb6ce886c9d5ace3135
long-lx <long-lx@users.noreply.github.com>
2022-06-25 11:05:57 +0700

    update aws s3

--- terraform/aws/01_s3/main.tf
+++ terraform/aws/01_s3/main.tf
@@ -1,4 +1,15 @@
 # khai báo provider, xác thực
+# https://registry.terraform.io/providers/hashicorp/aws/latest
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.20.1"
+    }
+  }
+}
+
 provider "aws" {
   // access_key = "**********"
   // secret_key = "**********"
@@ -7,6 +18,7 @@ provider "aws" {
 
 # tạo s3 bucket
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "devops-techmaster-03-longlx"
   acl    = "private"
